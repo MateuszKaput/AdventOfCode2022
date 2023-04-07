a9743c2f957ac86e61d09cb2707444fd2421bf5a
Mateusz <kaput.inf@gmail.com>
2023-04-08 00:12:22 +0200

    part two in challenge 9 done

--- src/codoadvento2022/ChallengeDay9.java
+++ src/codoadvento2022/ChallengeDay9.java
@@ -3,7 +3,9 @@ package codoadvento2022;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class ChallengeDay9 {
@@ -18,6 +20,7 @@ public class ChallengeDay9 {
 	
 	private static int partOneResult(String path) {
 		HashSet<String> visitedPositions = new HashSet<>();
+		
 		try {
 			File myInput = new File(path);
 			Scanner scanner = new Scanner(myInput);
@@ -87,18 +90,68 @@ public class ChallengeDay9 {
 	}
 	
 	private static int partTwoResult(String path) {
+		HashSet<String> visitedPositions = new HashSet<>();
+		HashMap<Integer,Point> allPoints = new HashMap<>();
+		
+		for (int i = 0; i < 10; i++) {allPoints.put(i,new Point(0,0));}
+		
 		try {
 			File myInput = new File(path);
 			Scanner scanner = new Scanner(myInput);
-
 			while(scanner.hasNextLine()) {
 				String data = scanner.nextLine();
+				String[] oneMove = data.split(" ");
+				for (int j = 0; j < Integer.parseInt(oneMove[1]); j++) {
+					for(int i=0; i<allPoints.size(); i++) {
+						if(i==0) {
+							if(oneMove[0].equals("L"))allPoints.get(0).x--;
+							if(oneMove[0].equals("U"))allPoints.get(0).y++;
+							if(oneMove[0].equals("R"))allPoints.get(0).x++;
+							if(oneMove[0].equals("D"))allPoints.get(0).y--;
+						}else {
+							int xDiff = allPoints.get(i-1).x-allPoints.get(i).x;
+							int yDiff = allPoints.get(i-1).y-allPoints.get(i).y;							
+							if(xDiff==2) {
+								allPoints.get(i).x++;
+								if(yDiff==1) allPoints.get(i).y++;
+								if(yDiff==-1)allPoints.get(i).y--;
+							}
+							if(xDiff==-2) {
+								allPoints.get(i).x--;
+								if(yDiff==1) allPoints.get(i).y++;
+								if(yDiff==-1)allPoints.get(i).y--;
+							}
+							if(yDiff==2) {
+								allPoints.get(i).y++;
+								if(xDiff==1) allPoints.get(i).x++;
+								if(xDiff==-1)allPoints.get(i).x--;
+							}
+							if(yDiff==-2) {
+								allPoints.get(i).y--;
+								if(xDiff==1) allPoints.get(i).x++;
+								if(xDiff==-1)allPoints.get(i).x--;
+							}
+						}
+					}
+					String position = allPoints.get(9).x+","+allPoints.get(9).y;
+					visitedPositions.add(position);
+				}
 			}
 			scanner.close();
 		}catch (FileNotFoundException e){
 			System.out.println("No file");
 			e.printStackTrace();
 		}
-		return 0;
+		return visitedPositions.size();
+	}
+		
+	public static class Point {
+	    public int x = 0;
+	    public int y = 0;
+	    
+	    public Point(int a, int b) {
+	        x = a;
+	        y = b;
+	    }
 	}
 }
