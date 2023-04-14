8edadde43f88c1548e2e731989fe7c1b0d843287
Mateusz <kaput.inf@gmail.com>
2023-04-14 16:37:50 +0200

    refractoring code

--- src/codoadvento2022/ChallengeDay11.java
+++ src/codoadvento2022/ChallengeDay11.java
@@ -16,64 +16,76 @@ import java.util.Scanner;
 
 public class ChallengeDay11 {
 	public static void main(String[] args) {
-		
+
 		String inputFile = "./resources/InputDay11";
-		partTwoResult(inputFile);
-			
+		int result2 = partOneResult(inputFile);
+		System.out.println("Part one result: " + result2);
+		BigInteger result1 = partTwoResult(inputFile);
+		System.out.println("Part two result: " + result1);
 	}
-	
-	private static int partTwoResult(String path) {
+
+	private static int partOneResult(String path) {
 		List<Monkey> monkeys = getApes(path);
 
-		Map<Integer,Monkey> monkeyMap = new HashMap<>();
+		Map<Integer, Monkey> monkeyMap = new HashMap<>();
 		for (Monkey monkey1 : monkeys) {
-            monkeyMap.put(monkey1.getId(), monkey1);
-        }
-		
-		for (int i = 0; i < 10000; i++) {
-            if (i == 1 || i == 20 || i == 1000 ||
-                i == 2000 || i == 3000 || i == 4000 ||
-                i == 5000 || i == 6000 || i == 7000 ||
-                i == 8000 || i == 9000 || i == 10000
-            ) {
-                System.out.println("== After round " + i + " ==");
-                for (Monkey monkey1 : monkeys) {
-                    monkey1.printInspections();
-                }
-                System.out.println();
-            }
-
-            for (Monkey monkey1 : monkeys) {
-                monkey1.doRotation(monkeyMap);
-            }
-        }
-		
-		List<Integer> inspections = new ArrayList<>();
-		for(Monkey ape : monkeys) {
-			inspections.add(ape.getChecks());
-			ape.printInspections();
+			monkeyMap.put(monkey1.getId(), monkey1);
 		}
-		
-		inspections.sort((a,b) -> b - a);
-		
-		System.out.println(BigInteger.valueOf(inspections.get(0)).multiply(BigInteger.valueOf(inspections.get(1))));
-		return 0;
+
+		for (int i = 0; i < 20; i++) {
+			for (Monkey monkey1 : monkeys) {
+				monkey1.doRotation(monkeyMap, "one");
+			}
+		}
+
+		List<Integer> inspections = new ArrayList<>();
+		for (Monkey ape : monkeys) {
+			inspections.add(ape.getChecks());
+		}
+
+		inspections.sort((a, b) -> b - a);
+		int output = inspections.get(0) * inspections.get(1);
+		return output;
 	}
-	
-	public static List<Monkey> getApes(String pathFile){
-		
+
+	private static BigInteger partTwoResult(String path) {
+		List<Monkey> monkeys = getApes(path);
+
+		Map<Integer, Monkey> monkeyMap = new HashMap<>();
+		for (Monkey monkey1 : monkeys) {
+			monkeyMap.put(monkey1.getId(), monkey1);
+		}
+
+		for (int i = 0; i < 10000; i++) {
+			for (Monkey monkey1 : monkeys) {
+				monkey1.doRotation(monkeyMap, "two");
+			}
+		}
+
+		List<Integer> inspections = new ArrayList<>();
+		for (Monkey ape : monkeys) {
+			inspections.add(ape.getChecks());
+		}
+
+		inspections.sort((a, b) -> b - a);
+		BigInteger output = BigInteger.valueOf(inspections.get(0)).multiply(BigInteger.valueOf(inspections.get(1)));
+		return output;
+	}
+
+	public static List<Monkey> getApes(String pathFile) {
+
 		Monkey monkey = null;
 		List<Monkey> monkeys = new ArrayList<>();
 		int counter = 0;
-		
+
 		try {
 			File myInput = new File(pathFile);
 			Scanner scanner = new Scanner(myInput);
-			while(scanner.hasNextLine()) {
+			while (scanner.hasNextLine()) {
 				String data = scanner.nextLine();
 				String[] splitedApe = data.split(":");
-				
-				switch(counter % 7) {
+
+				switch (counter % 7) {
 				case 0:
 					String monkeyNumber = splitedApe[0].split(" ")[1];
 					monkey = new Monkey(monkeyNumber);
@@ -87,9 +99,9 @@ public class ChallengeDay11 {
 					break;
 				case 2:
 					String operator = splitedApe[1].split(" ")[4];
-					if(splitedApe[1].split(" ")[5].equals("old")) {
+					if (splitedApe[1].split(" ")[5].equals("old")) {
 						operator = "^";
-					}else {
+					} else {
 						monkey.addOperatorValue(splitedApe[1].split(" ")[5]);
 					}
 					monkey.addOperatorChar(operator);
@@ -109,12 +121,12 @@ public class ChallengeDay11 {
 				case 6:
 					break;
 				default:
-					System.out.println("Something went wrong: "+counter);
+					System.out.println("Something went wrong: " + counter);
 				}
 				counter++;
 			}
 			scanner.close();
-		}catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			System.out.println("No file");
 			e.printStackTrace();
 		}
