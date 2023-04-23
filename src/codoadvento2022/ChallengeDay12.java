df761cd94e1c1278b9e18236ca6947256408a308
Mateusz <kaput.inf@gmail.com>
2023-04-23 14:13:37 +0200

    challenge 12 done

--- src/codoadvento2022/ChallengeDay12.java
+++ src/codoadvento2022/ChallengeDay12.java
@@ -2,209 +2,83 @@ package codoadvento2022;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ChallengeDay12 {
-
-	static String inputFile = "./resources/InputDay12";
-	static List<String[]> rowsOfMap = getList(inputFile);
-	static int maxRow = rowsOfMap.size();
-	static int maxCol = rowsOfMap.get(0).length;
-
-	static Node[][] node = new Node[maxRow][maxCol];
-	static Node startNode;
-	static Node goalNode;
-	static Node currentNode;
-	static ArrayList<Node> openList = new ArrayList<>();
-	static ArrayList<Node> checkedList = new ArrayList<>();
-
-	static boolean goalReached = false;
-
-	public static void main(String[] args) {
-
-		for (int i = 0; i < rowsOfMap.size(); i++) {
-			for (int j = 0; j < rowsOfMap.get(i).length; j++) {
-				int ascii = (int) rowsOfMap.get(i)[j].charAt(0);
-				if (ascii == 83) {
-					Node singleNode = new Node(i, j, 97);
-					node[i][j] = singleNode;
-					setStartNode(i, j);
-				} else if (ascii == 69) {
-					Node singleNode = new Node(i, j, 122);
-					node[i][j] = singleNode;
-
-					setGoalNode(i, j);
-				} else {
-					Node singleNode = new Node(i, j, ascii);
-					node[i][j] = singleNode;
-				}
-			}
-		}
-		setCostOfNodes();
-		search();
-	}
-
-	public static void search() {
-
-		while (goalReached == false) {
-			int row = currentNode.row;
-			int col = currentNode.col;
-			currentNode.setAsChecked();
-			checkedList.add(currentNode);
-			openList.remove(currentNode);
-
-			if (row - 1 >= 0) {
-				openNode(node[row - 1][col]);
-			}
-			if (col - 1 >= 0) {
-				openNode(node[row][col - 1]);
-			}
-			if (row + 1 < maxRow) {
-				openNode(node[row + 1][col]);
-			}
-			if (col + 1 < maxCol) {
-				openNode(node[row][col + 1]);
-			}
-
-			int bestNodeIndex = 0;
-			int bestNodeCost = 9999;
-
-			for (int i = 0; i < openList.size(); i++) {
-				if (openList.get(i).fCost < bestNodeCost) {
-					bestNodeIndex = i;
-					bestNodeCost = openList.get(i).fCost;
-				}
-			}
-			currentNode = openList.get(bestNodeIndex);
-
-			if (currentNode == goalNode) {
-				goalReached = true;
-			}
-		}
-		trackThePat();
-	}
-
-	private static void openNode(Node node) {
-		if (node.open == false && node.checked == false && node.value - currentNode.value < 2) {
-			node.setAsOpen();
-			node.parent = currentNode;
-			openList.add(node);
-		}
-	}
-
-	public static class Node {
-		Node parent = null;
-		int value;
-		int col;
-		int row;
-		int gCost;
-		int hCost;
-		int fCost;
-		boolean start;
-		boolean goal;
-		boolean open;
-		boolean checked;
-
-		public Node(int row, int col, int value) {
-			this.col = col;
-			this.row = row;
-			this.value = value;
-		}
-
-		public void setAsStart() {
-			start = true;
-		}
-
-		public void setAsGoal() {
-			goal = true;
-		}
-
-		public void setAsChecked() {
-			checked = true;
-		}
-
-		public void setAsOpen() {
-			open = true;
-		}
-	}
-
-	private static void setStartNode(int row, int col) {
-		node[row][col].setAsStart();
-		startNode = node[row][col];
-		currentNode = startNode;
-	}
-
-	private static void setGoalNode(int row, int col) {
-		node[row][col].setAsGoal();
-		goalNode = node[row][col];
-	}
-
-	private static void getCost(Node node) {
-
-		int xDistanceS = Math.abs(node.col - startNode.col);
-		int yDistanceS = Math.abs(node.row - startNode.row);
-		node.gCost = xDistanceS + yDistanceS;
-		int xDistanceE = Math.abs(node.col - goalNode.col);
-		int yDistanceE = Math.abs(node.row - goalNode.row);
-		node.hCost = xDistanceE + yDistanceE;
-		node.fCost = node.gCost + node.hCost;
-	}
-
-	private static void setCostOfNodes() {
-		int col = 0;
-		int row = 0;
-		while (row < maxRow && col < maxCol) {
-			getCost(node[row][col]);
-			col++;
-			if (col == maxCol) {
-				col = 0;
-				row++;
-			}
-		}
-	}
-
-	private static void trackThePat() {
-		Node current = goalNode;
-		int pathLenght = 0;
-		do {
-			rowsOfMap.get(current.row)[current.col] = ".";
-			current = current.parent;
-			pathLenght++;
-		} while (current != startNode);
-
-		for (String[] node : rowsOfMap) {
-			for (String string : node) {
-				System.out.print(string+" ");
-			}
-			System.out.println("");
-		}
-		System.out.println("Lenght of path after " + pathLenght);
-	}
-
-	private static List<String[]> getList(String inputFile) {
-		List<String[]> returnList = new ArrayList<>();
-
+	static List<Integer> end = null;
+	static Map<List<Integer>,Set<List<Integer>>> nww;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		nww = new HashMap<>();
+		Map<List<Integer>, Integer> map = new HashMap<>();
+		String inputFile = "./resources/InputDay12";
 		File myInput = new File(inputFile);
-		Scanner scanner;
-		try {
-			scanner = new Scanner(myInput);
-			while (scanner.hasNextLine()) {
-				String data = scanner.nextLine();
-				returnList.add(data.split(""));
+		Scanner scanner = new Scanner(myInput);
+		List<Integer> start = null;
+		List<List<Integer>> starts = new ArrayList<>();
+		int x, y = 0;
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			for (x = 0; x < line.length(); ++x) {
+				char c = line.charAt(x);
+				if (c == 'S') {
+					c = 'a';
+					start = List.of(x, y);
+				} else if (c == 'E') {
+					c = 'z';
+					end = List.of(x, y);
+				}
+				if(c=='a') {
+					starts.add(List.of(x,y));
+				}
+				map.put(List.of(x, y), c - 'a');
+				nww.put(List.of(x, y), new HashSet<>());
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			++y;
 		}
-		return returnList;
+		
+		for(List<Integer> node : map.keySet()) {
+            x = node.get(0);
+            y = node.get(1);
+            for(List<Integer> coords : List.of(List.of(0, 1), List.of(1, 0), List.of(0, -1), List.of(-1, 0))) {
+                int xx = x + coords.get(0);
+                int yy = y + coords.get(1);
+                List<Integer> newCoords = List.of(xx, yy);
+                if(map.containsKey(newCoords) && map.get(newCoords) <= map.get(node) + 1)
+                    nww.get(node).add(newCoords);
+            }
+        }
+		System.out.println(bfs(List.of(start)));
+		System.out.println(bfs(starts));
 	}
+	static int bfs(List<List<Integer>> starts) {
+        Queue<List<Integer>> q = new LinkedList<>(starts);
+        Map<List<Integer>, Integer> dist = new HashMap<>();
+        for(List<Integer> start : starts) {
+            dist.put(start, 0);
+        }
+        while(!q.isEmpty()) {
+        	List<Integer> node = q.poll();
+            if(node.equals(end)) {
+                return dist.get(node);
+            }
+            for(List<Integer> nb : nww.get(node)) {
+                if(!dist.containsKey(nb)) {
+                    dist.put(nb, dist.get(node) + 1);
+                    q.add(nb);
+                }
+            }
+        }
+        return -1;
+    }
+	
+	
 }
